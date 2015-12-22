fbe9a93f000b608c1bf76a9d4aba9d54c1bfe1c1
Walter Anderson <walter@kimbia.com>
2015-12-22 14:50:20 -0600

    adding css toggle for fields

--- app/components/settings/field.vue
+++ app/components/settings/field.vue
@@ -1,11 +1,43 @@
 <template>
 <div class="c-field">
-  <label>{{ field.label }}</label>
+
+  <label class="switch-light switch-material  c-field__display">
+    <input type="checkbox" v-model="field.display">
+    <span><a></a></span>
+  </label>
+
+  <label class="c-field__label">{{ field.label }}</label>
+
 </div>
 </template>
 
 <script>
 export default {
-  props: ['field']
+  props: ['field'],
+
+  methods: {
+  }
 }
 </script>
+
+<style lang="scss">
+@import '../../shared';
+
+.c-field {
+  background-color: #efefef;
+  padding: 15px;
+  width: 50%;
+  margin: auto;
+  border: 1px solid #e2e2e2;
+  border-radius: 2px;
+  margin-bottom: 15px;
+}
+
+.c-field__display {
+  float: left;
+}
+
+.c-field__label {
+  margin: 3px 10px;
+}
+</style>
