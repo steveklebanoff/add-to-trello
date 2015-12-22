44e5bb0d340745ab46c5454f6b16b93d8e7c557c
Walter Anderson <walter@kimbia.com>
2015-12-22 14:18:32 -0600

    adding field vue

---
+++ app/components/settings/field.vue
@@ -0,0 +1,11 @@
+<template>
+<div class="c-field">
+  <label>{{ field.label }}</label>
+</div>
+</template>
+
+<script>
+export default {
+  props: ['field']
+}
+</script>
--- app/components/settings/fields-form.vue
+++ app/components/settings/fields-form.vue
@@ -1,16 +1,23 @@
 <template>
 <div class="c-fields-form">
+  <field v-for="field in fields" :field="field"></field>
 </div>
 </template>
 
 <script>
 import fieldService from '../../lib/field/field.service'
+import FieldView from './field.vue'
 
 export default {
+
   data() {
     return {
       fields: fieldService.get()
     }
+  },
+
+  components: {
+    field: FieldView
   },
 
   methods: {
