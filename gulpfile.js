37799de897ad006d87a7807d88faa5ae13f9d1e2
Walter Anderson <walter@kimbia.com>
2016-01-16 23:22:37 -0600

    adding a build step to update the manifest and zip the build dir

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,8 @@
 var elixir = require('laravel-elixir');
+var gulp = require('gulp');
+var zip = require('gulp-zip');
+var fs = require('fs');
+var argv = require('yargs').argv;
 
 require('laravel-elixir-vueify');
 
@@ -10,22 +14,42 @@ elixir.config.css.folder = '';
 elixir(function(mix) {
     mix.browserify([
       'app/index.js'
-    ], 'dist/js/build.js');
+    ], 'build/js/build.js');
 
     mix.scripts([
       'node_modules/jquery/dist/jquery.js',
       'node_modules/bootstrap-sass/assets/javascripts/bootstrap.js',
       'node_modules/bootstrap-datepicker/dist/js/bootstrap-datepicker.js',
       'app/lib/trello/trello.client.min.js'
-    ], 'dist/js/vendor.js');
+    ], 'build/js/vendor.js');
 
     mix.copy([
         'manifest.json',
         'app/popup.html',
         'app/settings.html'
-    ], 'dist');
+    ], 'build');
 
-    mix.copy('app/images', 'dist/images');
+    mix.copy('app/images', 'build/images');
 
-    mix.copy('node_modules/bootstrap-sass/assets/fonts', 'dist/fonts');
+    mix.copy('node_modules/bootstrap-sass/assets/fonts', 'build/fonts');
 });
+
+gulp.task('build', ['default'], function() {
+  if (getVersion()) {
+    updateManifest();
+  }
+
+  return gulp.src('build/**/*', { base: '.' })
+    .pipe(zip('build.zip'))
+    .pipe(gulp.dest('./'));
+});
+
+function updateManifest() {
+  var manifest = JSON.parse(fs.readFileSync('./manifest.json', 'utf8'));
+  manifest.version = getVersion()
+  fs.writeFileSync('./manifest.json', JSON.stringify(manifest), 'utf8')
+}
+
+function getVersion() {
+  return argv.version || argv.v
+}
